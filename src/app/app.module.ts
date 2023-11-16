48a45a44573d42319ab6f39a99a5f1b9466c8619
leonardo.bocon <leonardo.bocon@manticore-labs.com>
2023-11-16 03:16:42 -0500

    feat:proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { PrimengModule } from './primeng/primeng.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BooksComponent } from './pages/books/books.component';
+import { AuthorsComponent } from './pages/authors/authors.component';
+import { EditorialsComponent } from './pages/editorials/editorials.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RolesComponent } from './pages/roles/roles.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    BooksComponent,
+    AuthorsComponent,
+    EditorialsComponent,
+    CategoriesComponent,
+    LoginComponent,
+    RegisterComponent,
+    RolesComponent,
+    NavComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PrimengModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
