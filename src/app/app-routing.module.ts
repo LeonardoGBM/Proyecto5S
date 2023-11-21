eaf868c039ce4fc27c277cd55ca867305f3ac548
leonardo.bocon <leonardo.bocon@manticore-labs.com>
2023-11-21 03:30:50 -0500

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent}
+  { path: '', component: LoginComponent},
+  { path: 'dashboard', component:DashboardComponent },
+  { path: 'login', component:LoginComponent }
+
 ];
 
 @NgModule({
